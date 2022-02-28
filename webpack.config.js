37f59267878ed09b7a0e0662332e18fb27589dc3
gcerv93 <gcerv93@protonmail.com>
2022-02-27 18:55:16 -0600

    css linking works with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'development',
@@ -9,9 +10,18 @@ module.exports = {
       title: 'To-do List',
       template: './src/index.html'
     }),
+    new MiniCssExtractPlugin( { filename: 'styles.css' }),
   ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
   },
 };
