ea2d52f01a8ea2d92d38471ec60d81aa0004c5f4
gcerv93 <gcerv93@protonmail.com>
2022-02-27 18:48:13 -0600

    working html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'To-do List',
+      template: './src/index.html'
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
