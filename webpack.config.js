3b1c8a694a59f875a753827a9b1e099288467d15
gcerv93 <gcerv93@protonmail.com>
2022-03-07 20:26:49 -0600

    build with babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,41 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
+  mode: "production",
+  entry: "./src/index.js",
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'TodoOrNotTodo',
-      template: './src/index.html'
+      title: "TodoOrNotTodo",
+      template: "./src/index.html",
     }),
-    new MiniCssExtractPlugin( { filename: 'styles.css' }),
+    new MiniCssExtractPlugin({ filename: "styles.css" }),
   ],
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
       },
     ],
   },
